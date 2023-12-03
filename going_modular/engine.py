9111241f853aa1de2ebe39d18b40adcd3c3425e3
Hossein Khosravipour <hossein.kh7935@gmail.com>
2023-12-03 16:58:29 +0330

    change model

--- going_modular/engine.py
+++ going_modular/engine.py
@@ -66,10 +66,11 @@ def one_step_test(model,
         y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
         test_acc += ((y_pred_class == y).sum().item())/len(y_pred)
 
-        print(f'batch : {batch}')
+        # if batch==2:
+        #     break
 
-    train_loss = train_loss/len(test_dataloader)
-    train_acc = train_acc/len(test_dataloader)
+    test_loss = test_loss/len(test_dataloader)
+    test_acc = test_acc/len(test_dataloader)
 
     return test_loss, test_acc
 
@@ -91,25 +92,25 @@ def train(model,
     
     for epochs in range(epochs):
     
-        # train_loss , train_acc = one_step_train(model=model,
-        #                                         train_dataloader=train_dataloader,
-        #                                         loss_fn=loss_fn,
-        #                                         optimizer=optimizer,
-        #                                         device=device)
+        train_loss , train_acc = one_step_train(model=model,
+                                                train_dataloader=train_dataloader,
+                                                loss_fn=loss_fn,
+                                                optimizer=optimizer,
+                                                device=device)
         
         test_loss , test_acc = one_step_test(model=model,
                                                 test_dataloader=test_dataloader,
                                                 loss_fn=loss_fn,
                                                 device=device)
-        # results['train_loss'].append(train_loss)
-        # results['train_acc'].append(train_acc)
+        results['train_loss'].append(train_loss)
+        results['train_acc'].append(train_acc)
         results['test_loss'].append(test_loss)
         results['test_acc'].append(test_acc)
 
         print(
           f"Epoch: {epochs+1} | "
-        #   f"train_loss: {train_loss:.4f} | "
-        #   f"train_acc: {train_acc:.4f} | "
+          f"train_loss: {train_loss:.4f} | "
+          f"train_acc: {train_acc:.4f} | "
           f"test_loss: {test_loss:.4f} | "
           f"test_acc: {test_acc:.4f}"
         )
