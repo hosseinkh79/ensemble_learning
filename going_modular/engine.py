a75fd2c919ff1e1b392c05e903553ac1aa710f02
Hossein Khosravipour <hossein.kh7935@gmail.com>
2023-12-03 15:33:23 +0330

    first commit

---
+++ going_modular/engine.py
@@ -0,0 +1,124 @@
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from sklearn.model_selection  import StratifiedKFold
+from torch.utils.data import Subset
+from torch.utils.data import DataLoader
+
+
+def one_step_train(model,
+                   train_dataloader, 
+                   loss_fn,
+                   optimizer,
+                   device):
+    
+    model = model.to(device)
+    model.train()
+
+    train_loss, train_acc = 0, 0
+
+    for batch, (X, y) in enumerate(train_dataloader):
+
+        optimizer.zero_grad()
+
+        X, y = X.to(device), y.to(device)
+
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+
+        optimizer.zero_grad()
+
+        loss.backward()
+        optimizer.step()
+
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += ((y_pred_class == y).sum().item())/len(y_pred)
+
+        if batch % 5 == 0 :
+            print(f'train_loss {loss}')
+            # print(f'train_acc {train_acc}')
+
+    train_loss = train_loss/len(train_dataloader)
+    train_acc = train_acc/len(train_dataloader)
+
+    return train_loss, train_acc
+
+
+def one_step_test(model,
+                  test_dataloader, 
+                  loss_fn, 
+                  device):
+    
+    model = model.to(device)
+    model.eval()
+
+    test_loss, test_acc = 0, 0
+
+    for batch, (X, y) in enumerate(test_dataloader):
+
+        X, y = X.to(device), y.to(device)
+
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        test_loss += loss.item()
+
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        test_acc += ((y_pred_class == y).sum().item())/len(y_pred)
+
+        print(f'batch : {batch}')
+
+    train_loss = train_loss/len(test_dataloader)
+    train_acc = train_acc/len(test_dataloader)
+
+    return test_loss, test_acc
+
+
+def train(model,
+          train_dataloader,
+          test_dataloader,
+          loss_fn,
+          optimizer,
+          device,
+          epochs):
+    
+    results = {
+        'train_loss':[],
+        'train_acc':[],
+        'test_loss':[],
+        'test_acc':[]
+    }
+    
+    for epochs in range(epochs):
+    
+        # train_loss , train_acc = one_step_train(model=model,
+        #                                         train_dataloader=train_dataloader,
+        #                                         loss_fn=loss_fn,
+        #                                         optimizer=optimizer,
+        #                                         device=device)
+        
+        test_loss , test_acc = one_step_test(model=model,
+                                                test_dataloader=test_dataloader,
+                                                loss_fn=loss_fn,
+                                                device=device)
+        # results['train_loss'].append(train_loss)
+        # results['train_acc'].append(train_acc)
+        results['test_loss'].append(test_loss)
+        results['test_acc'].append(test_acc)
+
+        print(
+          f"Epoch: {epochs+1} | "
+        #   f"train_loss: {train_loss:.4f} | "
+        #   f"train_acc: {train_acc:.4f} | "
+          f"test_loss: {test_loss:.4f} | "
+          f"test_acc: {test_acc:.4f}"
+        )
+        
+        
+
+
+
+
+
+
+
